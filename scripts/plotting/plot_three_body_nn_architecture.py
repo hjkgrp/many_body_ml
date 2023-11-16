97fb31671e167ef8ed6346eb3bb212bd426d67e1
Ralf Meyer <meyer.ralf@yahoo.com>
2023-11-16 11:43:03 -0500

    Plot of three-body nn architecture

---
+++ scripts/plotting/plot_three_body_nn_architecture.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+from mbeml.nn_models_functional import build_three_body_model
+
+
+if __name__ == "__main__":
+    model = build_three_body_model(
+        two_body_units=[64, 64],
+        three_body_units=[64, 64],
+        num_outputs=4,
+    )
+    tf.keras.utils.plot_model(
+        model,
+        to_file="plots/three_body_nn_architecture.png",
+        dpi=200,
+        expand_nested=True,
+        show_shapes=True,
+        show_layer_activations=True,
+        show_layer_names=True,
+    )
