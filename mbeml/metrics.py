d2542c6c60065ee6bc9cb774da92051a55f47724
Ralf Meyer <meyer.ralf@yahoo.com>
2024-03-13 11:29:12 -0400

    Update mean_negative_log_likelihood metric

--- mbeml/metrics.py
+++ mbeml/metrics.py
@@ -6,6 +6,7 @@ __all__ = ["mean_absolute_error", "r2_score"]
 
 
 def mean_negative_log_likelihood(y_true, y_mean, y_std):
+    y_true, y_mean, y_std = y_true.flatten(), y_mean.flatten(), y_std.flatten()
     return np.mean(
         ((y_true - y_mean) ** 2) / (2 * y_std**2)
         - np.log(1 / np.sqrt(2 * np.pi * y_std**2))
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -3,12 +3,19 @@ from scipy.stats import norm
 from mbeml.metrics import mean_negative_log_likelihood
 
 
+def naive_nll(y_true, y_mean, y_std):
+    nll = np.zeros(len(y_true))
+    for i, (true_mean, pred_mean, pred_std) in enumerate(zip(y_true, y_mean, y_std)):
+        nll[i] = -norm.logpdf(true_mean, loc=pred_mean, scale=pred_std)
+    return np.mean(nll)
+
+
 def test_mean_negative_log_likelihood():
     y_true = np.array([6.4, 1.0, -1.2, 40.2])
     y_mean = np.array([8.2, -0.2, -0.9, 38.4])
     y_std = np.array([1.2, 0.8, 1.1, 1.4])
 
-    ref = np.mean(-np.log(norm.pdf(y_true, loc=y_mean, scale=y_std)))
+    ref = naive_nll(y_true, y_mean, y_std)
     mnll = mean_negative_log_likelihood(y_true, y_mean, y_std)
 
     np.testing.assert_allclose(mnll, ref)
@@ -19,7 +26,18 @@ def test_mean_negative_log_likelihood_reshaped():
     y_mean = np.array([8.2, -0.2, -0.9, 38.4]).reshape(-1, 1)
     y_std = np.array([1.2, 0.8, 1.1, 1.4]).reshape(-1, 1)
 
-    ref = np.mean(-np.log(norm.pdf(y_true, loc=y_mean, scale=y_std)))
+    ref = naive_nll(y_true, y_mean, y_std)
+    mnll = mean_negative_log_likelihood(y_true, y_mean, y_std)
+
+    np.testing.assert_allclose(mnll, ref)
+
+
+def test_mean_negative_log_likelihood_mixed_shape():
+    y_true = np.array([6.4, 1.0, -1.2, 40.2]).reshape(-1, 1)
+    y_mean = np.array([8.2, -0.2, -0.9, 38.4])
+    y_std = np.array([1.2, 0.8, 1.1, 1.4])
+
+    ref = naive_nll(y_true, y_mean, y_std)
     mnll = mean_negative_log_likelihood(y_true, y_mean, y_std)
 
     np.testing.assert_allclose(mnll, ref)
